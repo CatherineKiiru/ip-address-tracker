10e93f975c3aac6cae4703005351e473911e2ece
Catherine <66266208+CatherineKiiru@users.noreply.github.com>
2024-01-15 10:59:06 +0300

    fix: fix background image dimension

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,8 @@
 
 const Header = () => {
   return (
-    <section className="bg-mobile-image bg-no-repeat bg-cover md:bg-desktop-image p-24 md:p-36 ">
-      <header className="text-white ">
+    <section className="bg-mobile-image bg-no-repeat bg-cover md:bg-desktop-image h-auto max-w-full ">
+      <header className="text-white pt-14">
         Ip Address Tracker
       </header>
       <div>
