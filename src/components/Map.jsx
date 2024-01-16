fc44c24bd3eae8a404d368ee66f106d42151337c
Catherine <66266208+CatherineKiiru@users.noreply.github.com>
2024-01-16 21:29:53 +0300

    add mapbox

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,20 @@
-const Map = () => {
-  return (
-    <div>Map</div>
-  )
-}
+import mapboxgl from "mapbox-gl";
 
-export default Map
+// eslint-disable-next-line no-unused-vars
+const Map = (address, loading, success, error) => {
+  mapboxgl.accessToken = "<your access token here>";
+  const map = new mapboxgl.Map({
+    container: "map", 
+    style: "mapbox://styles/mapbox/streets-v12", 
+    center: [-74.5, 40], 
+    zoom: 9, 
+  });
+
+
+
+  return <div>
+    
+  </div>;
+};
+
+export default Map;
