ce8e38734caa221926115d6ca350c71d11bd1a33
Catherine <66266208+CatherineKiiru@users.noreply.github.com>
2024-01-16 21:15:10 +0300

    define objects and add to address component

--- src/components/Address.jsx
+++ src/components/Address.jsx
@@ -1,28 +1,48 @@
 import LoadingMessage from "./loadingMessage";
-import Spinner from './Spinner'
+import Spinner from "./Spinner";
 
-//The address component needs to have three states: the loading state, the rendered address, and an error state
-// eslint-disable-next-line no-unused-vars
-const Address = (address, loading, error) => {
+const addressObj = {
+  ip: "",
+  location: "",
+  timezone: "",
+  isp: "",
+}
+addressObj();
+
+const Address = (address, loading, success, error) => {
   return (
     <>
       <section className="absolute mx-auto left-0 right-0 max-w-sm md:max-w-xl lg:max-w-5xl top-[20rem] bg-white rounded-[1rem] shadow-md">
-        {/* define loading states and error boundaries */}
         {loading && (
           <div className="flex gap-4 items-center">
-            <Spinner/>
+            <Spinner />
             <LoadingMessage />
           </div>
         )}
-        <main className="block md:flex justify-center text-center py-12">
-          <div>Address</div>
-          <div>location</div>
-          <div>Timezone</div>
-          <div>ISP</div>
-        </main>
+        {!loading && error == 1 && (
+          <div className="text-2xl font-bold text-center text-vDarkGray animate-bounce">
+            Failed To Fetch Data. Check Domain or IP Address
+          </div>
+        )}
+        {success && error == 0 && (
+          <main className="block md:flex justify-center text-center py-12">
+            <div>Address</div>
+            <p>{address.ip}</p>
+            <div>location</div>
+            <p>{address.location}</p>
+            <div>Timezone</div>
+            <p>{address.timezone}</p>
+            <div>ISP</div>
+            <p>{address.ISP}</p>
+          </main>
+        )}
       </section>
     </>
   );
 };
 
 export default Address;
+
+
+//The address component needs to have three states: the loading state, the rendered address, and an error state
+// eslint-disable-next-line no-unused-vars
