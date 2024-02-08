a9f325f507d51612a223ef0b9924755612e91dd4
Catherine <66266208+CatherineKiiru@users.noreply.github.com>
2024-02-08 18:07:25 +0300

    refactor: changed input to inpuilt input element

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -27,7 +27,7 @@ const Header = () => {
   useEffect(() => {
     fetchData
   }, [addressData])
-   
+
   return (
     <>
       <section className="relative p-24 bg-mobile-image bg-no-repeat bg-cover md:bg-desktop-image h-auto">
@@ -37,17 +37,22 @@ const Header = () => {
 
         
         <Paper
-          component="form"
+          
+          elevation={6}
           sx={{ borderRadius: 3 }}
           className="max-w-sm md:max-w-lg flex mx-auto"
         >
-          <InputBase
+          <input className="w-full rounded-xl px-4"
+          type="text" 
+          placeholder="Search for any IP address or domain"
+          />
+          {/* <InputBase
             sx={{ ml: 2, flex: 1 }}
             placeholder="Search for any IP address or domain"
-            type="text"
+            type=""
             value={input}
             onChange={setInput}
-          />
+          /> */}
 
           <div className="bg-veryDarkGray rounded-r-xl">
             <IconButton sx={{ p: 1.5 }} aria-label="directions">
