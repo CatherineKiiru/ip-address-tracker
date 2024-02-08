26391c342324b8acd8e8532bc6559163d1736ec3
Catherine <66266208+CatherineKiiru@users.noreply.github.com>
2024-02-08 09:19:32 +0300

    defined function to fetch data

--- src/components/AddressBox.jsx
+++ src/components/AddressBox.jsx
@@ -1,42 +1,65 @@
-// import { useState } from "react";
-import LoadingMessage from "./loadingMessage";
-import Spinner from "./Spinner";
+/* eslint-disable no-undef */
+import { useEffect, useState } from "react";
+// import LoadingMessage from "./loadingMessage";
+// import Spinner from "./Spinner";
+
+// const address = {
+//   ip: "102.219.210.194",
+//   location: "Nairobi",
+//   timezone: "EAT",
+//   ISP: "Vijiji Connect",
+// };
 
 const AddressBox = () => {
-  const address = {
-    ip: "102.219.210.194",
-    location: "Nairobi",
-    timezone: "EAT",
-    ISP: "Vijiji Connect",
-  };
+  const [addressData, setAddressData] = useState([]);
+  const url = "https://geo.ipify.org/api/v2/"
+  const API_KEY = "at_yziwQqxxgNwkeIJWAW8ugR6JfNCam"
+  
+  const fetchData = async () => {
+    const response = await fetch (url, {
+      headers: {
+        method: POST,
+        API_KEY,
+      }
+    }) 
+    const data = response.json()
+    
+    console.log(data) 
+    setAddressData(data)
+  }
 
+  useEffect(() => {
+    fetchData
+  }, [])
+
+  
+  
   const pClasses = 'font-semibold text-2xl'
   const hClasses = 'text-darkGray uppercase font-semibold'
 
-  // const [addressDetails, setAddressDetails] = useState(addressObj())
   return (
     <>
       <section className="p-12 absolute mx-auto justify-center items-center right-0 left-0 max-w-sm md:max-w-xl lg:max-w-5xl top-[20rem] bg-white rounded-[1rem] shadow-md">
         {
-          <main className="mx-auto gap-4 grid md:grid-cols-4  md:divide-x-2 md:justify-between text-center">
+          <main className="mx-auto gap-4 grid md:grid-cols-4 md:divide-x-2 md:justify-between text-center">
             <div className="">
               <h1 className={hClasses}>IP Address</h1>
-              <p className={pClasses}>{address.ip}</p>
+              <p className={pClasses}>{addressData.ip}</p>
             </div>
 
             <div>
               <h1 className={hClasses}>location</h1>
-              <p className={pClasses}>{address.location}</p>
+              <p className={pClasses}>{addressData.location}</p>
             </div>
 
             <div>
               <h1 className={hClasses}>Timezone</h1>
-              <p className={pClasses}>{address.timezone}</p>
+              <p className={pClasses}>{addressData.timezone}</p>
             </div>
 
             <div>
               <h1 className={hClasses}>ISP</h1>
-              <p className={pClasses}>{address.ISP}</p>
+              <p className={pClasses}>{addressData.ISP}</p>
             </div>
           </main>
         }
@@ -47,11 +70,9 @@ const AddressBox = () => {
 
 export default AddressBox;
 
-// Define a function or a variable for the map API
-// We've defined an address object that takes in the properties we'll assign the values to display
-// Define an error function
-// The app requires 3 states:
-// 1. a state to handle the users input which can either be a url or an IP address
-// 2. A state to handle the posiition n latitude and longitude
-// 3 .A state that takes in the address details. It can take in an address object
-// 4. A state for handling the ipAddress
+// Pseudo code refresher
+// We've defined the addressbox function
+// this function is what will contain the url and api key
+// the url and api key contains the data we want to fetch from ipify
+// we're fetching the data with the async await method
+// We're also using the useEffect hook to 
