09c937f2b029993fd1d78b3f45a1d7c47707d9fa
Catherine <66266208+CatherineKiiru@users.noreply.github.com>
2024-02-08 10:51:13 +0300

    defined states for map container and initialized useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import Header from './components/Header'
 import AddressBox from './components/AddressBox'
-
+import Map from './components/Map'
 
 const App = () => {
   return (
     <div>
       <Header/>
       <AddressBox/>
+      <Map />
     </div>
   )
 }
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,18 +1,27 @@
-import mapboxgl from "mapbox-gl";
+import mapboxgl from "mapbox-gl"; // or "const mapboxgl = require('mapbox-gl');"
+import React, { useRef, useEffect, useState } from "react";
 
-// eslint-disable-next-line no-unused-vars
-const Map = (address, loading, success, error) => {
-  mapboxgl.accessToken = "<your access token here>";
-  const map = new mapboxgl.Map({
-    container: "map", 
-    style: "mapbox://styles/mapbox/streets-v12", 
-    center: [-74.5, 40], 
-    zoom: 9, 
-  });
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiY2F0ZWRvZSIsImEiOiJjbHNjdXFyd3IwM3p2Mmtwdmkza21tbGlmIn0.M-3t1R3CMk19Yukht7H5gA";
 
-  return <div>
-    
-  </div>;
-};
+export default function Map() {
+  // create default states for rendering the map when the user interacts with it
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+  const [zoom, setZoom] = useState(9);
 
-export default Map;
+  // Initialize the map with useEffect. This code will be invoked after the app is inserted in the DOM tree
+useEffect(() => {
+  if (!mapContainer.current) return;
+  map.current = new mapboxgl.Map({
+    container: mapContainer.current,
+    style: 'mapbox://styles/mapbox/streets-v12',
+    center: [lng, lat],
+    zoom: zoom
+  })
+
+})
+
+}
